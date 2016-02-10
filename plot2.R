0d9d377e9089d043c05a427caf6d45b3f8750b1c
paulgid <paulgid@gmail.com>
2016-02-10 10:54:00 +0100

    loaded lubridate and fixed wrong variable name

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-
+library(lubridate)
 if(!file.exists('household_power_consumption.txt')) {
   stop('You not the source data for this program. You can get them at https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip.') 
 }
@@ -9,7 +9,7 @@ data$DateTime <- strptime(paste(data$Date, data$Time), '%d/%m/%Y %X')
 dateFrom <- as.POSIXct("2007-02-01 00:00:00")
 dateTo <- as.POSIXct("2007-02-02 00:00:00")
 interval <- interval(dateFrom, dateTo)
-filtered = data[data$DateTime %within% int,]
+filtered = data[data$DateTime %within% interval,]
 
 png('plot2.png', width=480, height=480, bg="white")
 
